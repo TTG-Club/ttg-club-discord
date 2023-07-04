7f9af33a31f97af1de5dc3267c51b8a9e4b57d5f
svifty7 <svifty7@gmail.com>
2023-07-04 18:41:40 +0300

    fix(restart): try hard restart app on error

--- src/events/index.ts
+++ src/events/index.ts
@@ -1,4 +1,11 @@
+import eventError from './error';
 import eventInteractionCreate from './interactionCreate';
 import eventReady from './ready';
+import eventShardError from './shardError';
 
-export default [eventReady, eventInteractionCreate];
+export default [
+  eventReady,
+  eventInteractionCreate,
+  eventError,
+  eventShardError
+];
