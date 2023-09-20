d2ebc52640f94fbf236eccdc86c9bd881c260c71
svifty7 <svifty7@gmail.com>
2023-09-21 02:55:52 +0300

    fixes

--- src/utils/useDiceRoller.ts
+++ src/utils/useDiceRoller.ts
@@ -24,7 +24,7 @@ export function useDiceRoller() {
       result: roll.value,
       highest: highest!.value,
       lowest: lowest!.value,
-      full: `**[${notation}]**: ${renderer.render(roll)}`
+      full: `**[${notation}]:** ${renderer.render(roll)}`
     };
   };
 
@@ -33,7 +33,7 @@ export function useDiceRoller() {
 
     return {
       result: roll.value,
-      full: `**[${notation}]**: ${renderer.render(roll)}`
+      full: `**[${notation}]:** ${renderer.render(roll)}`
     };
   };
 
