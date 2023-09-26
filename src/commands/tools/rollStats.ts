d20282ed1f973ad1793a7397f45ca4ff2bd71093
svifty7 <svifty7@gmail.com>
2023-09-26 20:25:45 +0300

    fixes

--- src/commands/tools/rollStats.ts
+++ src/commands/tools/rollStats.ts
@@ -1,9 +1,11 @@
+import { colord } from 'colord';
 import { EmbedBuilder, SlashCommandBuilder } from 'discord.js';
 
 import { useDiceRoller } from '../../utils/useDiceRoller.js';
 
 import type { SlashCommand } from '../../types.js';
 
+const COLOR_COEFFICIENT = 1.2;
 const { getDiceMsg } = useDiceRoller();
 
 const commandRollStats: SlashCommand = {
@@ -19,13 +21,19 @@ const commandRollStats: SlashCommand = {
       );
 
       const total = rolls.reduce((result, roll) => result + roll.value, 0);
+      const totalPercent = Math.round((total / 108) * 100);
+
+      const hueCorrected =
+        Math.round((totalPercent * COLOR_COEFFICIENT) / 10) * 10;
+
+      const color = colord(`hsl(${hueCorrected},100%,50%)`).toRgb();
 
       const results = rolls.map(
         (roll, index) => `${index + 1}. [${roll.notation}]: ${roll.rendered}`
       );
 
       embed
-        .setColor('#3567C9')
+        .setColor([color.r, color.g, color.b])
         .addFields({
           name: 'Броски характеристик',
           value: results.join('\n'),
